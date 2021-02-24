f4f335b224bcd16750c4e9324e48a8781341f945
eric-d-culver <eric.d.culver@gmail.com>
2021-02-24 14:06:33 -0700

    fixed some bugs, ran testing examples from minecraft wiki. Note that not all enchantments are included yet, but including them only requires editing the enchantment_data dictionary at top of file

--- anvil_calc.py
+++ anvil_calc.py
@@ -138,7 +138,7 @@ enchantment_data = {
         }
 }
 
-incompitible_enchantments_data = [
+incompatible_enchantments_data = [
         ["Sharpness", "Smite", "Bane of Arthropods"],
         ["Fortune", "Silk Touch"],
         ["Protection", "Fire Protection", "Blast Protection", "Projectile Protection"],
@@ -179,6 +179,8 @@ def compatible(target_type, enchant):
     return target_type in enchantment_data[enchant]["type_apply"]
 
 def incompatible_enchants(enchant1, enchant2):
+    if enchant1 == enchant2:
+        return False # enchantment is always compatible with itself
     incompatible = False
     for category in incompatible_enchantments_data:
         if enchant1 in category and enchant2 in category:
@@ -188,17 +190,21 @@ def incompatible_enchants(enchant1, enchant2):
 def enchantment_cost(target, sacrifice):
     cost = 0
     for enchant, lvl in sacrifice["enchantments"].items():
+        #print("Considering enchantment", enchant, "at level", lvl)
         if compatible(target["type"], enchant):
+            #print(enchant, "is compatible with target")
+            incompatible = False
             for enchant_target in target["enchantments"]:
-                incompatible = False
                 if incompatible_enchants(enchant_target, enchant):
+                    #print("Target has incompatible enchantment", enchant_target)
                     incompatible = True
                     cost += 1
             if incompatible:
-                break
+                continue
             if enchant in target["enchantments"]:
                 # combining enchantments
-                target_lvl = target["enchantments"]["enchant"]
+                target_lvl = target["enchantments"][enchant]
+                #print("Target has same enchantment at level", target_lvl)
                 if lvl > target_lvl:
                     final_lvl = lvl
                 elif lvl == target_lvl:
@@ -210,12 +216,14 @@ def enchantment_cost(target, sacrifice):
                     final_lvl = target_lvl
             else: # enchantment not on target
                 final_lvl = lvl
+            #print(enchant, "at level", final_lvl, "has cost", final_lvl*enchant_mult(enchant, sacrifice["type"]))
             cost += final_lvl*enchant_mult(enchant, sacrifice["type"])
+    return cost
 
 def combine_enchantments(target, sacrifice):
-    res_enchants = copy(target["enchantments"])
-    for enchant, lvl in sacrifice["enchantments"]:
-        if compatitible(target["type"], enchant):
+    res_enchants = dict(target["enchantments"])
+    for enchant, lvl in sacrifice["enchantments"].items():
+        if compatible(target["type"], enchant):
             for enchant_target in target["enchantments"]:
                 incompatible = False
                 if incompatible_enchants(enchant_target, enchant):
@@ -238,3 +246,90 @@ def combine_enchantments(target, sacrifice):
                 final_lvl = lvl
             res_enchants[enchant] = final_lvl
     return res_enchants
+
+example1_target = {
+        "type": "sword",
+        "uses": 0,
+        "enchantments": {
+            "Sharpness": 3,
+            "Knockback": 2,
+            "Looting": 3
+        }
+    }
+
+example1_sacrifice = {
+        "type": "sword",
+        "uses": 0,
+        "enchantments": {
+            "Sharpness": 3,
+            "Looting": 3
+        }
+    }
+
+example2_target = {
+        "type": "sword",
+        "uses": 0,
+        "enchantments": {
+            "Sharpness": 3,
+            "Knockback": 2,
+            "Looting": 1
+        }
+    }
+
+example2_sacrifice = {
+        "type": "sword",
+        "uses": 0,
+        "enchantments": {
+            "Sharpness": 1,
+            "Looting": 3
+        }
+    }
+
+example3_target = {
+        "type": "sword",
+        "uses": 0,
+        "enchantments": {
+            "Sharpness": 2,
+            "Looting": 2
+        }
+    }
+
+example3_sacrifice = {
+        "type": "sword",
+        "uses": 0,
+        "enchantments": {
+            "Smite": 5,
+            "Looting": 2
+        }
+    }
+
+example4_target = {
+        "type": "sword",
+        "uses": 0,
+        "enchantments": {
+            "Looting": 2
+        }
+    }
+
+example4_sacrifice = {
+        "type": "book",
+        "uses": 0,
+        "enchantments": {
+            "Protection": 3,
+            "Sharpness": 1,
+            "Looting": 3
+        }
+    }
+
+test = True
+
+def testing_examples(target, sacrifice, val1, val2):
+    print(combine(target, sacrifice))
+    print(enchantment_cost(target, sacrifice), "should be", val1)
+    print(enchantment_cost(sacrifice, target), "should be", val2)
+
+if test:
+    testing_examples(example1_target, example1_sacrifice, 16, 20)
+    testing_examples(example2_target, example2_sacrifice, 15, 19)
+    testing_examples(example3_target, example3_sacrifice, 13, 13)
+    testing_examples(example4_target, example4_sacrifice, 7, 0)
